018b022ba044b7bf6f84083eadcfbb543f3ae9e4
LI NAN <linanvae@163.com>
2019-08-12 21:38:44 +0900

    改了下数据准备

--- ex_trac.sh
+++ ex_trac.sh
@@ -13,6 +13,6 @@ echo "Finish data prepare!"
 date
 ########################################################
 
-python2.7 pre_process_data.py calculate_train_features --train_speech_path="train.txt" --data_type=train
-python2.7 pre_process_data.py calculate_train_features --train_speech_path="data/cv/inputs_dir.txt" --data_type=cv
+python2.7 pre_process_data.py calculate_train_features --train_speech_path="data/tr/inputs.txt" --data_type=train
+python2.7 pre_process_data.py calculate_train_features --train_speech_path="data/cv/inputs.txt" --data_type=cv
 python2.7 pre_process_test.py calculate_train_features --train_speech_path="data/test/real/rev_dir.txt" --data_type=test/real
